12d60c2e73ce0f48d9122f5c63dbcbcd850c5cb5
Sven <glaglasven@live.com>
2016-09-22 17:37:00 +0200

    Snap to Grid #1

--- ModularHomeDesign/MainPage.xaml.cs
+++ ModularHomeDesign/MainPage.xaml.cs
@@ -26,35 +26,20 @@ namespace ModularHomeDesign
     /// </summary>
     public sealed partial class MainPage : Page
     {
-		private TranslateTransform dragTranslation;
-		private List<Polygon> listOfPolygon;
+		private Dictionary<Polygon, TranslateTransform> listOfPolygon;
+		public static double GridHeight = 10;
+		public static double GridWidth = 10;
 
 		public MainPage()
         {
             this.InitializeComponent();
-			listOfPolygon = new List<Polygon>();
+			listOfPolygon = new Dictionary<Polygon, TranslateTransform>();
 			PopulateOneRoom();
         }
 
 		private void PopulateOneRoom()
 		{
-			dragTranslation = new TranslateTransform();
-			Polygon line = new Polygon()
-			{
-				Points = RoomCompute.GetPoints(new Room.Room(0)),
-				IsRightTapEnabled = true,
-				Stroke = new SolidColorBrush(Colors.Black),
-				StrokeThickness = 6,
-				ManipulationMode = ManipulationModes.All,
-				RenderTransform = this.dragTranslation,
-				Fill = new SolidColorBrush(Colors.Transparent)
-			};
-			line.RightTapped += Line_RightTapped;
-			line.ManipulationDelta += Line_ManipulationDelta;
-			Canvas.SetLeft(line, 50);
-			Canvas.SetTop(line, 200);
-			listOfPolygon.Add(line);
-			Plan.Children.Add(line);
+			AddStdBox();
 		}
 
 		private void Line_RightTapped(object sender, RightTappedRoutedEventArgs e)
@@ -63,18 +48,66 @@ namespace ModularHomeDesign
 			Debug.WriteLine(e.GetPosition((Polyline)sender).ToString());
 		}
 
-
-		//TestRectangle.ManipulationDelta += Drag_ManipulationDelta;
-		//	dragTranslation = new TranslateTransform();
-		//TestRectangle.RenderTransform = this.dragTranslation;
-
 		void Line_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
 		{
 			// Move the rectangle.
-			dragTranslation.X += e.Delta.Translation.X;
-			dragTranslation.Y += e.Delta.Translation.Y;
-			foreach(Polygon child in listOfPolygon)
-				child.TransformToVisual(Window.Current.Content).TransformPoint(new Point(0,0));
+			foreach(KeyValuePair<Polygon, TranslateTransform> item in listOfPolygon)
+			{
+				if (item.Key == sender)
+				{
+					TranslateTransform a = item.Value as TranslateTransform;
+					a.X += e.Delta.Translation.X;
+					a.Y += e.Delta.Translation.Y;
+					break;
+				}
+			}
+		}
+
+		private void Line_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+		{
+			//Stick to grid
+			Polygon child = sender as Polygon;
+			Point position = child.TransformToVisual(Window.Current.Content).TransformPoint(new Point(0, 0));
+			Point newPoint = RoomCompute.SnapGridPoint(position);
+			foreach (KeyValuePair<Polygon, TranslateTransform> item in listOfPolygon)
+			{
+				if (item.Key == sender)
+				{
+					TranslateTransform a = item.Value as TranslateTransform;
+					a.X += newPoint.X;
+					a.Y += newPoint.Y;
+					break;
+				}
+			}
+			Debug.WriteLine("Was at X: {0}, Y : {1}\nNow at X: {2}, Y : {3}",position.X,position.Y, newPoint.X, newPoint.Y);
+		}
+
+		private void Add_Click(object sender, RoutedEventArgs e)
+		{
+			AddStdBox();
+		}
+
+		private void AddStdBox()
+		{
+			TranslateTransform tmpTransform = new TranslateTransform();
+
+			Polygon line = new Polygon()
+			{
+				Points = RoomCompute.GetPoints(new Room.Room(0)),
+				IsRightTapEnabled = true,
+				Stroke = new SolidColorBrush(Colors.Black),
+				StrokeThickness = 6,
+				ManipulationMode = ManipulationModes.All,
+				RenderTransform = tmpTransform,
+				Fill = new SolidColorBrush(Colors.Transparent)
+			};
+			line.RightTapped += Line_RightTapped;
+			line.ManipulationDelta += Line_ManipulationDelta;
+			line.ManipulationCompleted += Line_ManipulationCompleted;
+			Canvas.SetLeft(line, 50);
+			Canvas.SetTop(line, 50);
+			listOfPolygon.Add(line, tmpTransform);
+			Plan.Children.Add(line);
 		}
 	}
 }
--- ModularHomeDesign/Room/RoomCompute.cs
+++ ModularHomeDesign/Room/RoomCompute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
@@ -19,6 +20,47 @@ namespace ModularHomeDesign.Room
 			points.Add(new Point(a.left + a.width, a.top + a.height));
 			points.Add(new Point(a.left + a.width, a.top));
 			return points;
+		}
+		
+		/// <summary>
+		/// Get the absolute distance between two points
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double GetDistance(Point a, Point b)
+		{
+			return Math.Sqrt(Math.Pow(GetDistanceX(a, b), 2) + Math.Pow(GetDistanceY(a, b), 2));
+		}
+
+		/// <summary>
+		/// Get the absolute distance on x axis between two points
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double GetDistanceX(Point a, Point b)
+		{
+			return Math.Abs(a.X - b.X);
+		}
+
+		/// <summary>
+		/// Get th absolute distance on y axis between two points
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double GetDistanceY(Point a, Point b)
+		{
+			return Math.Abs(a.Y - b.Y);
+		}
+
+		public static Point SnapGridPoint(Point original)
+		{
+			double snapX = Math.Round(original.X / MainPage.GridWidth) * MainPage.GridWidth;
+			double snapY = Math.Round(original.Y / MainPage.GridHeight) * MainPage.GridHeight;
+
+			return new Point(snapX, snapY);
 		}
 	}
 }
